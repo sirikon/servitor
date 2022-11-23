f4b0400c2f08c24d9665e98affbf300e65f607fd
Carlos Fdez. Llamas <hello@sirikon.me>
2022-11-23 19:34:45 +0100

    remove unused events

--- controller/src/modules/core/events/EventBus.ts
+++ controller/src/modules/core/events/EventBus.ts
@@ -3,9 +3,6 @@ import EventEmitter from "eventemitter3";
 export type EventBus = EventEmitter<EventTypes>;
 
 export type EventTypes = {
-  "seed-execution-created": (args: { id: number }) => void;
-  "seed-execution-started": (args: { id: number }) => void;
-  "seed-execution-ended": (args: { id: number }) => void;
   "shutdown": () => void;
 };
 
