f4a3b01667b79b20ebc1c1939347c31bfae75df6
Carlos Fdez. Llamas <hello@sirikon.me>
2022-11-23 23:04:53 +0100

    stream events to ui

--- controller/src/modules/app/web/WebApplication.ts
+++ controller/src/modules/app/web/WebApplication.ts
@@ -1,6 +1,7 @@
 import { Application, Router } from "oak/mod.ts";
 import { SeedLogStorage, seedLogStorage } from "@/core/seed/SeedLogStorage.ts";
 import { seedActions } from "@/core/seed/SeedActions.ts";
+import { SeedExecution, seedStore } from "../../core/seed/SeedStore.ts";
 
 export class WebApplication {
   constructor(
@@ -18,6 +19,27 @@ export class WebApplication {
 
     this.router.post("/api/seed/execute", async (ctx) => {
       ctx.response.body = await seedActions.createExecution();
+    });
+
+    this.router.post("/api/seed/executions/subscribe", (ctx) => {
+      const textEncoder = new TextEncoder();
+      let close = () => {};
+      const reader = new ReadableStream<Uint8Array>({
+        start(controller) {
+          const handler = ({ execution }: { execution: SeedExecution }) => {
+            const chunk = textEncoder.encode(JSON.stringify(execution) + "\n");
+            controller.enqueue(chunk);
+          };
+          seedStore.events.on("execution-updated", handler);
+          close = () => seedStore.events.off("execution-updated", handler);
+        },
+        cancel() {
+          close();
+        },
+      });
+      ctx.response.headers.set("content-type", "text/plain");
+      ctx.response.headers.set("x-content-type-options", "nosniff");
+      ctx.response.body = reader;
     });
 
     this.router.get("/api/seed/executions/:id/logs", async (ctx) => {
