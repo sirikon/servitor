0ca0441179cb91f6b765f0486a06a91e338af1b9
Carlos Fdez. Llamas <hello@sirikon.me>
2023-12-11 21:05:52 +0100

    tweak

--- servitor.py
+++ servitor.py
@@ -6,15 +6,15 @@ class ServitorWebServer(BaseHTTPRequestHandler):
     def do_GET(self):
         self.reply_json(200, {"message": "hello world"})
 
+    def reply_json(self, code: int, body: object):
+        self.reply(code, "application/json", json.dumps(body).encode())
+
     def reply(self, code: int, type: str, body: bytes):
         self.send_response(code)
         self.send_header("Content-Type", type)
         self.send_header("Content-Length", str(len(body)))
         self.end_headers()
         self.wfile.write(body)
-
-    def reply_json(self, code: int, body: object):
-        self.reply(code, "application/json", json.dumps(body).encode())
 
 
 httpd = HTTPServer(("", 8000), ServitorWebServer)
