560f33f6466de02473314a62bb72fa9e0b28b33b
Carlos Fdez. Llamas <hello@sirikon.me>
2023-12-13 13:35:15 +0100

    tweak

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 root="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
-python_cmd="$(command -v python || command -v python3)"
+python_cmd="$(command -v python3 || command -v python)"
 (
     cd "${root}/example"
     export PYTHONPATH="${root}/src"
