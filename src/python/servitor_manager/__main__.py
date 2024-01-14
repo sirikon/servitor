f2be664667806e8d3c65edccb3fcf3e79343b70c
Carlos Fdez. Llamas <hello@sirikon.me>
2024-01-14 19:11:46 +0100

    wip

--- src/python/servitor_manager/__main__.py
+++ src/python/servitor_manager/__main__.py
@@ -7,44 +7,56 @@ import os.path
 
 
 def main():
-    args = get_args()
+    args = cli()
 
-    print_systemd_file(
-        {
-            "Unit": {
-                "Description": "Servitor",
-            },
-            "Service": {
-                "User": args.user,
-                "Type": "simple",
-                "Environment": "PYTHONPATH="
-                + os.path.normpath(
-                    os.path.join(os.path.dirname(__file__), "../../src/python")
-                ),
-                "WorkingDirectory": "idunno",
-                "ExecStart": f"{args.python} -m servitor",
-                "Restart": "always",
-                "After": "network.target",
-            },
-            "Install": {"WantedBy": "multi-user.target"},
-        }
-    )
+    match args.subcommand:
+        case "gen-systemd-service":
+            print_systemd_file(
+                {
+                    "Unit": {
+                        "Description": "Servitor",
+                    },
+                    "Service": {
+                        "User": args.user,
+                        "Type": "simple",
+                        "Environment": "PYTHONPATH="
+                        + os.path.normpath(
+                            os.path.join(os.path.dirname(__file__), "../../src/python")
+                        ),
+                        "WorkingDirectory": "idunno",
+                        "ExecStart": f"{args.python} -m servitor",
+                        "Restart": "always",
+                        "After": "network.target",
+                    },
+                    "Install": {"WantedBy": "multi-user.target"},
+                }
+            )
+        case _:
+            print("Unknown subcommand " + args.subcommand)
 
 
-def get_args():
+def cli():
     parser = argparse.ArgumentParser(prog="servitor_manager")
-    parser.add_argument(
+    subparsers = parser.add_subparsers()
+    gen_sysd_parser = subparsers.add_parser("gen-systemd-service")
+    gen_sysd_parser.set_defaults(subcommand="gen-systemd-service")
+
+    gen_sysd_parser.add_argument(
         "-u",
         "--user",
         help="user to use. defaults to current user",
         default=os.getlogin(),
     )
-    parser.add_argument(
+    gen_sysd_parser.add_argument(
         "-p",
         "--python",
         help="python command to use. defaults to 'python3'",
         default="python3",
     )
+
+    if len(sys.argv) == 1:
+        parser.print_help(sys.stderr)
+        sys.exit(1)
     return parser.parse_args()
 
 
