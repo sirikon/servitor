4471b801c124dbc44833b2f3bdbae25c081b4f27
Carlos Fdez. Llamas <hello@sirikon.me>
2024-01-21 18:39:56 +0100

    now is installer

--- installer.sh
+++ installer.sh
@@ -2,8 +2,8 @@
 set -euo pipefail
 
 root="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
-SYSTEM_ROOT="$root/scripts/servitor-manager-test"
+SYSTEM_ROOT="$root/scripts/servitor-installer-test"
 mkdir -p "$SYSTEM_ROOT"
 
 export SYSTEM_ROOT
-"$root/scripts/servitor-manager.sh" "$@"
+"$root/scripts/servitor-installer.sh" "$@"
