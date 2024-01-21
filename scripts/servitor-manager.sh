f1dd683b5bd9ab4f3f068da8099757ddc238b3d1
Carlos Fdez. Llamas <hello@sirikon.me>
2024-01-21 17:54:31 +0100

    wip

--- scripts/servitor-manager.sh
+++ scripts/servitor-manager.sh
@@ -1,21 +1,29 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-REPOSITORY="github.com/sirikon/servitor"
-
 SYSTEM_ROOT="${SYSTEM_ROOT:-}"
+SERVITOR_REPOSITORY="github.com/sirikon/servitor"
 SERVITOR_ROOT="${SYSTEM_ROOT}/opt/servitor"
+SERVITOR_USER="servitor"
 
 function main() {
     log "Installing Servitor"
 
     if [ ! -d "$SERVITOR_ROOT" ]; then
         log "Servitor repository not found. Cloning."
-        git clone "https://${REPOSITORY}" "$SERVITOR_ROOT"
+        git clone "https://${SERVITOR_REPOSITORY}" "$SERVITOR_ROOT"
     else
         log "Servitor repository found. Pulling."
         (cd "$SERVITOR_ROOT" && git pull)
     fi
+
+    if ! id "$SERVITOR_USER" >/dev/null 2>&1; then
+        log "Servitor user not found. Creating"
+        useradd -h
+    else
+        echo 'user not found'
+    fi
+
 }
 
 function log {
